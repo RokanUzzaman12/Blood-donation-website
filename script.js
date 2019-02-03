369e8636df994909ea082cf0c886528a47d53f11
Md. Rokan Uzzaman <44034772+RokanUzzaman12@users.noreply.github.com>
2019-02-03 23:54:39 +0600

    commit 1

---
+++ script.js
@@ -0,0 +1,29 @@
+var index =1;
+slide(1);
+
+function pre(){
+    index=index-1;
+    slide(index);
+}
+function post(){
+    index=index+1;
+    slide(index);
+}
+
+function slide(n){
+    var x=document.getElementsByClassName("slideshow");
+    var i;
+    if(n>x.length){
+        index=1;
+    }
+
+    else if(n<1){
+        index=x.length;
+    }
+
+    for(i=0;i<x.length;i++){
+        x[i].style.display="none";
+    }
+    x[index-1].style.display="block";
+
+}
